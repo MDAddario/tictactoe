3c584da9a9dfeaaa37aafac29d6e8c6348f792ff
MDAddario <38199062+MDAddario@users.noreply.github.com>
2019-01-13 21:34:16 -0500

    Merged full_solve and optimal_play

--- full_solve.c
+++ full_solve.c
@@ -4,7 +4,6 @@
 * Completely solves game and generates game tree.
 *
 * AUTHOR: Michael Lindner-D'Addario
-* DATE: Jan 09 2019
 ***********************************************************/
 
 #include <stdio.h>
@@ -134,5 +133,63 @@ void free_ptr(int** ptr){
 
 	free(*ptr);
 	*ptr = NULL;
+	return;
+}
+
+void play_against_cpu(game* ttt, node* nd, int is_CPU_P1){
+
+	// Create game if start
+	if (ttt == NULL){
+		ttt = game_init();
+	}
+
+	print_game(ttt);
+
+	// Ensure game not over
+	if (!ttt->isActive)
+		return;
+
+	int row, col;
+
+	// Player turn
+	if (!is_CPU_P1){
+
+		printf("Its the players turn!\n");
+		printf("Please input row and column:\n");
+		scanf("%d %d", &row, &col);
+
+		while(make_move(ttt, row, col) < 0){
+			printf("Please input a valid move:\n");
+			scanf("%d %d", &row, &col);
+		}
+	}
+	// CPU turn
+	else{
+
+		printf("Its the CPUs turn!\n");
+
+		int move_found = 0;
+
+		for (int i = 0; i < DIMENSION; i++){
+			for (int j = 0; j < DIMENSION; j++){
+
+				if (nd->children[i][j] != NULL){
+
+					if (nd->children[i][j]->value == nd->value){
+						move_found = 1;
+						row = i;
+						col = j;
+						break;
+					}
+				}
+			if (move_found)
+				break;
+			}
+		}
+		printf("%d %d\n", row, col);
+		make_move(ttt, row, col);
+	}
+	play_against_cpu(ttt, nd->children[row][col], !is_CPU_P1);
+
 	return;
 }
